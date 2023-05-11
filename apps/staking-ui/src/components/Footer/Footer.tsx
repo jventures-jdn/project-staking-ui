a6719d9fd50082074f636a3bf3238d49d3491110
Vasin Sermsampan <dev.vasin@gmail.com>
2023-05-11 14:01:59 +0700

    docs(*): update readme

--- apps/staking-ui/src/components/Footer/Footer.tsx
+++ apps/staking-ui/src/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import './Footer.css'
+import packageJson from '../../../../../package.json'
 
 const Footer = () => {
   return (
     <div className="footer">
       <span>
-        V1.0 Copyright ©2023
-        <a href="https://www.jventures.co.th/"> JVentures</a>
+        V{packageJson.version} Copyright ©2023
+        <a href="https://www.jventures.co.th/"> {packageJson.author}</a>
       </span>
     </div>
   )
