7ef95c10e53260e2854420f9e8ba22f07783e358
Vasin Sermsampan <dev.vasin@gmail.com>
2023-05-25 14:03:38 +0700

    feat(ui.footer): add commit version

--- apps/staking-ui/src/components/Layout/Footer/Footer.tsx
+++ apps/staking-ui/src/components/Layout/Footer/Footer.tsx
@@ -5,7 +5,18 @@ const Footer = () => {
   return (
     <div className="footer">
       <span>
-        V{packageJson.version} Build 03, Copyright ©2023
+        V{packageJson.version} Commit:{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={`https://github.com/jventures-jdn/project-staking-ui/commit/${process.env.REACT_APP_COMMIT}`}
+        >
+          {`${process.env.REACT_APP_COMMIT?.slice(
+            0,
+            4,
+          )}...${process.env.REACT_APP_COMMIT?.slice(-4)}`}
+        </a>
+        , Copyright ©2023
         <a href="https://www.jventures.co.th/"> {packageJson.author}</a>
       </span>
     </div>
