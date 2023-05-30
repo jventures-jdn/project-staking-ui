49f209f610282674b0267be3531012868e582dcb
Vasin Sermsampan <dev.vasin@gmail.com>
2023-05-30 12:54:12 +0700

    fix(ui.wallet-connect): fix default chain

--- apps/staking-ui/src/stores/WalletConnectStore.ts
+++ apps/staking-ui/src/stores/WalletConnectStore.ts
@@ -8,13 +8,14 @@ import {
   polygon,
 } from 'wagmi/chains'
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { makeAutoObservable } from 'mobx'
+import { makeAutoObservable, toJS } from 'mobx'
 import {
   CHAIN_DECIMAL_UNIT,
   CHAIN_EXPLORER,
   CHAIN_ID,
   CHAIN_NAME,
   CHAIN_RPC,
+  EXPECT_CHAIN,
 } from '@utils/chain/src/chain'
 
 export default class WalletConnectStore {
@@ -30,50 +31,50 @@ export default class WalletConnectStore {
   constructor() {
     makeAutoObservable(this)
 
+    const jfin = {
+      id: CHAIN_ID.JFIN,
+      name: CHAIN_NAME.JFIN,
+      network: CHAIN_NAME.JFIN,
+      nativeCurrency: {
+        decimals: CHAIN_DECIMAL_UNIT,
+        name: CHAIN_NAME.JFIN,
+        symbol: CHAIN_NAME.JFIN,
+      },
+      rpcUrls: {
+        public: { http: [CHAIN_RPC.JFIN] },
+        default: { http: [CHAIN_RPC.JFIN] },
+      },
+      blockExplorers: {
+        default: {
+          name: 'BlockScout',
+          url: CHAIN_EXPLORER.JFIN,
+        },
+      },
+    }
+    const jfint = {
+      id: CHAIN_ID.JFINT,
+      name: CHAIN_NAME.JFINT,
+      network: CHAIN_NAME.JFINT,
+      nativeCurrency: {
+        decimals: CHAIN_DECIMAL_UNIT,
+        name: CHAIN_NAME.JFINT,
+        symbol: CHAIN_NAME.JFINT,
+      },
+      rpcUrls: {
+        public: { http: [CHAIN_RPC.JFINT] },
+        default: { http: [CHAIN_RPC.JFINT] },
+      },
+      blockExplorers: {
+        default: {
+          name: 'BlockScout',
+          url: CHAIN_EXPLORER.JFINT,
+        },
+      },
+    }
+
     // 1. Init jfin chain
     this.chains = [
-      // JFIN
-      {
-        id: CHAIN_ID.JFIN,
-        name: CHAIN_NAME.JFIN,
-        network: CHAIN_NAME.JFIN,
-        nativeCurrency: {
-          decimals: CHAIN_DECIMAL_UNIT,
-          name: CHAIN_NAME.JFIN,
-          symbol: CHAIN_NAME.JFIN,
-        },
-        rpcUrls: {
-          public: { http: [CHAIN_RPC.JFIN] },
-          default: { http: [CHAIN_RPC.JFIN] },
-        },
-        blockExplorers: {
-          default: {
-            name: 'BlockScout',
-            url: CHAIN_EXPLORER.JFIN,
-          },
-        },
-      },
-      // JFIN Test
-      {
-        id: CHAIN_ID.JFINT,
-        name: CHAIN_NAME.JFINT,
-        network: CHAIN_NAME.JFINT,
-        nativeCurrency: {
-          decimals: CHAIN_DECIMAL_UNIT,
-          name: CHAIN_NAME.JFINT,
-          symbol: CHAIN_NAME.JFINT,
-        },
-        rpcUrls: {
-          public: { http: [CHAIN_RPC.JFINT] },
-          default: { http: [CHAIN_RPC.JFINT] },
-        },
-        blockExplorers: {
-          default: {
-            name: 'BlockScout',
-            url: CHAIN_EXPLORER.JFINT,
-          },
-        },
-      },
+      ...(EXPECT_CHAIN.chainName === 'JFIN' ? [jfin, jfint] : [jfint, jfin]),
       mainnet,
       bsc,
       polygon,
@@ -83,6 +84,8 @@ export default class WalletConnectStore {
       goerli,
     ] as Chain[]
 
+    console.log(this.chains)
+
     // 2. Get configure from configureChains
     this.configure = configureChains(this.chains, [
       w3mProvider({ projectId: this.projectId }),
@@ -91,6 +94,7 @@ export default class WalletConnectStore {
     // 3. Create wagmiClient from web3modal
     this.wagmiClient = createClient({
       autoConnect: true,
+
       connectors: [
         ...w3mConnectors({
           projectId: this.projectId,
@@ -99,8 +103,12 @@ export default class WalletConnectStore {
         }),
       ],
       provider: this.configure.provider,
-      webSocketProvider: this.configure.webSocketProvider,
     })
+
+    this.wagmiClient.clearState()
+    console.log(toJS(this.wagmiClient.config.storage))
+
+    // this.wagmiClient.
 
     this.ethereumClient = new EthereumClient(this.wagmiClient, this.chains)
   }
