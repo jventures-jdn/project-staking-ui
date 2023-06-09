0041a58066db882e96cf1fe4615d1e3463237aa0
Nick <thitipong.nick@gmail.com>
2023-06-09 14:22:30 +0700

    add build script for cloudflare build command

---
+++ apps/staking-ui/build.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+# FOR CLOUDFLARE PAGES BUILD
+
+if [ "$CF_PAGES_BRANCH" == *"mainnet"* ]; then
+    pnpm build:jfin
+elif [ "$CF_PAGES_BRANCH" == *"testnet"* ]; then
+    pnpm build:jfintest
+else
+    echo Error : network not found in branch name
+fi
