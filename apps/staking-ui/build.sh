7d65d1ebe0d0e5c6b03d2aed1eee364f4aad6649
Nick <thitipong.nick@gmail.com>
2023-06-09 14:30:27 +0700

    fix build bash

--- apps/staking-ui/build.sh
+++ apps/staking-ui/build.sh
@@ -2,9 +2,11 @@
 
 # FOR CLOUDFLARE PAGES BUILD
 
-if [ "$CF_PAGES_BRANCH" == *"mainnet"* ]; then
+echo $CF_PAGES_BRANCH
+
+if [ $CF_PAGES_BRANCH =~ "mainnet" ]; then
     pnpm build:jfin
-elif [ "$CF_PAGES_BRANCH" == *"testnet"* ]; then
+elif [ $CF_PAGES_BRANCH =~ "testnet" ]; then
     pnpm build:jfintest
 else
     echo Error : network not found in branch name
